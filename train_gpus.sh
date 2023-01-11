4681d90a594ceb34bd5ae2dbefce8596593e3957
shiwk20 <shiwk20@mails.tsinghua.edu.cn>
2023-01-12 04:10:00 +0800

    update

--- train_gpus.sh
+++
@@ -1,3 +0,0 @@
-PORT=$(comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1)
-
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT train_gpus.py --config configs/triplet.yaml
