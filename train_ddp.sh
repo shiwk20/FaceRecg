4fc3e5cb4600f8143e03d9aeb8d77806182267e2
shiwk20 <shiwk20@qq.com>
2023-01-13 16:17:39 +0800

    update

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3,4 python -m torch.distributed.launch --nproc_per_node=2 --master_port=23757 train_ddp.py --type mtcnn
+CUDA_VISIBLE_DEVICES=1,2 python -m torch.distributed.launch --nproc_per_node=2 --master_port=23747 train_ddp.py --type landmark
