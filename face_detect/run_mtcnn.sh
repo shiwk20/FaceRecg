cf639e88ff9905beecbb013812ee5b833a822358
shiwk20 <shiwk20@qq.com>
2023-01-13 10:18:19 +0800

    uodate face_detect

--- face_detect/run_mtcnn.sh
+++ face_detect/run_mtcnn.sh
@@ -1,6 +1,6 @@
 for i in 0 1 2 3 4 5 6
 do
     echo $i
-    python face_detect/face_detect.py $i &
+    python face_detect/detect_mtcnn.py $i &
 done
-python face_detect/face_detect.py 7
+python face_detect/detect_mtcnn.py 7
